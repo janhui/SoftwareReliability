06d92d72e37447357a8446bfd0270c0ca0f2d5b8
Liam Barden <ltb11@edge30.doc.ic.ac.uk>
2014-10-23 12:39:54 +0100

    nearly have too large indexes working

--- bookings/SeatReservationManager.java
+++ bookings/SeatReservationManager.java
@@ -4,7 +4,10 @@ public class SeatReservationManager {
 
     //@ invariant seatReservations != null;
     //@ invariant (\forall int x; 0 <= x && x < seatReservations.length ==> seatReservations[x] != null);
-
+    
+    //@ invariant seatReservations.length == (Seat.MAX_ROW - Seat.MIN_ROW) + 1;
+    //@ invariant (\forall int x; 0 <= x && x < seatReservations.length ==> seatReservations[x].length  == (Seat.MAX_NUMBER - Seat.MIN_NUMBER + 1));
+    
     private final Customer[][] seatReservations;
     
     public SeatReservationManager() {
