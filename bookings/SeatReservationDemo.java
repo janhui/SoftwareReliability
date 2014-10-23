2a2e6ab782586988c083eed9bd3e3dd3fb8ee829
Liam Barden <edge06.doc.ic.ac.uk>
2014-10-23 16:48:36 +0100

    starting daikon

--- bookings/SeatReservationDemo.java
+++ bookings/SeatReservationDemo.java
@@ -3,7 +3,15 @@ package bookings;
 public class SeatReservationDemo {
 
     public static void main(String[] args) throws ReservationException {
-        
+        SeatReservationManager manager = new SeatReservationManager();
+        manager.reserve(new Seat('A', 1), new Customer());
+        manager.reserve(new Seat('C', 2), new Customer());
+        manager.reserve(new Seat('E', 13), new Customer());
+        manager.reserve(new Seat('D', 4), new Customer());
+        manager.reserve(new Seat('B', 19), new Customer());
+        manager.reserve(new Seat('F', 17), new Customer());
+            
     }
+    
     
 }
