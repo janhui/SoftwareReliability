cec7c7d2a4d0a2ee4aa6621dc5a7c863d24ffb6b
Liam Barden <ltb11@edge07.doc.ic.ac.uk>
2014-10-28 12:06:18 +0000

    updated daikon

--- bookings/SeatReservationDemo.java
+++ bookings/SeatReservationDemo.java
@@ -3,15 +3,42 @@ package bookings;
 public class SeatReservationDemo {
 
     public static void main(String[] args) throws ReservationException {
-        SeatReservationManager manager = new SeatReservationManager();
-        manager.reserve(new Seat('A', 1), new Customer());
-        manager.reserve(new Seat('C', 2), new Customer());
-        manager.reserve(new Seat('E', 13), new Customer());
-        manager.reserve(new Seat('D', 4), new Customer());
-        manager.reserve(new Seat('B', 19), new Customer());
-        manager.reserve(new Seat('F', 17), new Customer());
-        manager.reserve(new Seat('G', 20), new Customer());
+    
+       // Reserve every Seat.
+        SeatReservationManager manager1 = new SeatReservationManager();
+        for (char row = Seat.MIN_ROW; row <= Seat.MAX_ROW; row++) {
+            for (int number = Seat.MIN_NUMBER; number <= Seat.MAX_NUMBER; number++) {
+                manager1.reserve(new Seat(row, number), new Customer());
+            }
+        }
+        
+        // Unreserve every Seat.
+        for (char row = Seat.MIN_ROW; row <= Seat.MAX_ROW; row++) {
+            for (int number = Seat.MIN_NUMBER; number <= Seat.MAX_NUMBER; number++) {
+                manager1.unreserve(new Seat(row, number));
+            }
+        }
             
+        // reserveNextFree and printing.
+        SeatReservationManager manager2 = new SeatReservationManager();
+        int totalSeats = (Seat.MAX_ROW - Seat.MIN_ROW + 1) * (Seat.MAX_NUMBER - Seat.MIN_NUMBER + 1);
+        for (int i = 0; i < totalSeats; i++) {
+            manager2.reserveNextFree(new Customer());
+        }
+        System.out.println(manager2);
+        
+        // Exception handling.
+        SeatReservationManager manager3 = new SeatReservationManager();
+        manager3.reserve(new Seat('A', 1), new Customer());
+        
+        try {
+            manager3.reserve(new Seat('A', 1), new Customer());
+        } catch (ReservationException e) {}
+        
+        try {
+            manager3.unreserve(new Seat('A', 2));
+        } catch (ReservationException e) {}
+        
     }
     
     
